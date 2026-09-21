6d3f660abe1b7bc9224a8f4c9625843a7d602c64
ayushydv1805 <ayushyadav0000018@gmail.com>
2026-09-21 12:39:57 +0530

    refactor: normalize legacy service entry points

--- src/services/youtube.js
+++ src/services/youtube.js
@@ -1,18 +1,1 @@
-const API_KEY = import.meta.env.VITE_YOUTUBE_API_KEY;
-
-export async function getTravelVideos(place) {
-  try {
-    const response = await fetch(
-      `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${encodeURIComponent(
-        place + " travel guide"
-      )}&type=video&maxResults=4&key=${API_KEY}`
-    );
-
-    const data = await response.json();
-
-    return data.items || [];
-  } catch (err) {
-    console.error(err);
-    return [];
-  }
-}
+export { getTravelVideos } from "./media/youtube.js";
