a3b6bfc9f460a39ca30fd0870f6a9d97bbc83446
ayushydv1805 <ayushyadav0000018@gmail.com>
2026-09-21 15:45:18 +0530

    fix: update SearchBar autocomplete service import

--- src/components/search/SearchBar.jsx
+++ src/components/search/SearchBar.jsx
@@ -1,6 +1,6 @@
 import { useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { searchPlaces } from "../../services/autocomplete";
+import { searchPlaces } from "../../services/geospatial/autocomplete.js";
 
 function SearchBar() {
   const navigate = useNavigate();
