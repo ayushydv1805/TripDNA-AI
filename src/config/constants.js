19367607495ef576d2841ab96ab2411adf9ad9c9
ayushydv1805 <ayushyadav0000018@gmail.com>
2026-09-21 12:37:59 +0530

    refactor: centralize config and platform service modules

---
+++ src/config/constants.js
@@ -0,0 +1,6 @@
+export const APP_NAME = "TripDNA AI";
+export const MAX_RECENT_SEARCHES = 10;
+export const MAX_SAVED_TRIPS = 20;
+export const SEARCH_SUGGESTION_LIMIT = 5;
+export const NEARBY_PLACE_LIMIT = 10;
+export const GALLERY_IMAGE_LIMIT = 6;
