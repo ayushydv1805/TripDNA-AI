2ee65683e3bc46b88a54b6eccbe19eeea9486865
ayushydv1805 <ayushyadav0000018@gmail.com>
2026-09-21 12:38:47 +0530

    refactor: update pages to feature service modules

--- src/pages/SavedTrips.jsx
+++ src/pages/SavedTrips.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import Navbar from "../components/layout/Navbar";
-import { deleteTrip, getTrips } from "../services/trips";
+import { deleteTrip, getTrips } from "../services/storage/trips";
 
 function SavedTrips() {
   const [trips, setTrips] = useState([]);
